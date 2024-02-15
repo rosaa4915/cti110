3cf1876f0667e96e51f403543dc7c7629347c532
rosaa4915 <159969344+rosaa4915@users.noreply.github.com>
2024-02-15 14:36:45 -0500

    Add files via upload

---
+++ P1LAB1_RosaDannyv2.py
@@ -0,0 +1,14 @@
+#comments
+#P1LAB1
+#Rosa
+#Enter a name, get greeted
+
+
+fname = input("Enter your first name: ")
+lname = input ("Enter your last name: ")
+print(fname, lname)
+print( "Hello, " + fname,lname + "! Welcome to CTI-110!")
+input+("Press any key to exit")
+
+num1= int(input("Enter a number: ")
+num2= int(input("Enter a number: ")
